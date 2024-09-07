c90652a3553c67b7ce05e29c9c1ffd0b17f61601
UniBreakfast <unibreakfast1@gmail.com>
2024-09-07 20:49:48 +0300

    name expressions made with operators

--- index.js
+++ index.js
@@ -125,15 +125,181 @@ arguments  // access array-like object with function arguments
 //* compound expressions (multiple tokens):
 //* can be named accordingly to the last operator evaluated
 
-//? Even though all expressions return a value,
-//? many are used for side effects (assignments, increments)
-
 
 //* groups of expressions by operator category:
 // arithmetic
 // logical
 // bitwise
 // relational
-// equality
+// comparison
 // assignment
-// update value
+// call
+
+//? Even though all expressions return a value,
+//? many are used for side effects (assignments, increments)
+
+
+typeof a  // выражение определения типа значения
++a  // выражение приведения к числовому значению
+-a  // выражение обращения значения по знаку
+a + b  // выражение суммы
+a - b  // выражение вычитания
+a * b  // выражение произведения
+a / b  // выражение деления
+a % b  // выражение получения остатка при целочисленном делении
+a ** b  // выражение возведения в степень
+a = b  // выражение присвоения значения сущности
+a == b  // выражение проверки равенства с приведением типов
+a === b  // выражение проверки равенства значений
+a != b  // выражение проверки неравенства с приведением типов
+a !== b  // выражение проверки неравенства значений
+a < b; a > b  // выражения строгого сравнения
+a <= b; a >= b  // выражения нестрогого сравнения
+a += b  // выражение наращивания значения сущности
+a -= b  // выражение сокращения значения сущности
+a *= b  // выражение умножения значения сущности
+a /= b  // выражение деления значения сущности
+a %= b  // выражение замены значения сущности остатком целочисленного деления
+a **= b  // выражение возведения значения сущности в степень
+a && b  // выражение выбора значения логическим И
+a || b  // выражение выбора значения логическим ИЛИ
+a ?? b  // выражение выбора значения логическим ИЛИ по наличию значения
+a ? b : c  // выражение выбора значения согласно условию
+!a  // выражение логического отрицания
+++a  // выражение пред-увеличения значения сущности на единицу
+--a  // выражение пред-уменьшения значения сущности на единицу
+a++  // выражение пост-увеличения значения сущности на единицу
+a--  // выражение пост-уменьшения значения сущности на единицу
+a in b  // выражение проверки вхождения ключа в объект
+a.b  // выражение чтения значения свойства по идентификатору
+a?.b  // выражение чтения значения свойства по идентификатору при наличии носителя
+a[b]  // выражение чтения значения свойства по ключу
+a[b]  // выражение взятия значения из ячейки по индексу (массивы, псевдомассивы)
+a?.[b]  // выражение взятия значения по ключу (индексу) при наличии носителя
+a()  // выражение вызова функции
+a?.()  // выражение вызова функции при наличии таковой
+a instanceof b  // выражение проверки наследования
+a.b()  // выражение вызова метода по идентификатору
+a[b]()  // выражение вызова метода по ключу
+a.b = c  // выражение присвоения значения свойства по идентификатору
+a[b] = c  // выражение присвоения значения свойства по ключу
+delete a  // выражение удаления глобальной переменной
+delete a.b  // выражение удаления свойства по идентификатору
+delete a[b]  // выражение удаления свойства по ключу
+a(b); a(b, c)  // выражения вызова функции c аргументами
+a.b(c); a.b(c, d)  // выражения вызова метода c аргументами
+new A; new A()  // выражение создания экземпляра типа (класса)
+super()  // выражение инициализации экземпляра праконструктором
+import(a)  // выражение импорта модуля
+a << b  // выражение битового сдвига влево
+a >> b  // выражение битового сдвига вправо
+a >>> b  // выражение битового сдвига вправо с потерей знака
+a & b  // выражение битового И
+a | b  // выражение битового ИЛИ
+a ^ b  // выражение битового ИСКЛЮЧЕНИЯ
+~a  // выражение битового НЕ
+a <<= b  // выражение битового сдвига значения сущности влево
+a >>= b  // выражение битового сдвига значения сущности вправо
+a &= b  // выражение изменения значения сущности битовым И
+a |= b  // выражение изменения значения сущности битовым ИЛИ
+a ^= b;  // выражение изменения значения сущности битовым ИСКЛЮЧЕНИЕМ
+[...a]  // выражение создания массива со значениями итерируемого
+(
+{...a}  // выражение создания объекта со свойствами данного объекта
+)  
+a(...b)  // выражение вызова функции с высыпанием аргументов из итерируемого
+a.b(...c);  // выражение вызова метода с высыпанием аргументов из итерируемого
+[a] = b; [a, b] = c;  // выражения деструктуризации итерируемого
+[,, a] = b;  // выражение деструктуризации итерируемого, пропуская элементы в начале
+[a,...b] = c  // выражение деструктуризации итерируемого со сбором остальных значений в массив
+(
+{a} = b, {a, b} = c  // выражения деструктуризации объекта
+)(
+{a, ...b} = c  // выражение деструктуризации объекта со сбором остальных свойств в объект
+)(
+{a: b} = c  // выражение деструктуризации объекта c иным именем свойства/сущности
+)
+[[a]] = b; [{a}] = b, {a: [b]} = c, {a: {b}} = c  // выражение деструктуризации итерируемых и объектов вглубь
+
+
+typeof a  // type detection expression
++a  // coercion to number expression
+-a  // unary negation expression
+a + b  // addition expression
+a - b  // subtraction expression
+a * b  // multiplication expression
+a / b  // division expression
+a % b  // remainder expression
+a ** b  // exponentiation expression
+a = b  // assignment expression
+a == b  // equality expression with coercion to same type
+a === b  // strict equality expression
+a != b  // inequality expression with coercion to same type
+a !== b  // strict inequality expression
+a < b; a > b  // strict comparison expression
+a <= b; a >= b  // non-strict comparison expression
+a += b  // specified increment expression
+a -= b  // specified decrement expression
+a *= b  // specified product reassignment expression
+a /= b  // specified division reassignment expression
+a %= b  // specified remainder reassignment expression
+a **= b  // specified exponentiation reassignment expression
+a && b  // value selection by logical conjunction expression
+a || b  // value selection by logical disjunction expression
+a ?? b  // value selection by logical disjunction expression based on emptiness
+a ? b : c  // value selection by conditional expression
+!a  // logical negation expression
+++a  // pre-increment expression
+--a  // pre-decrement expression
+a++  // post-increment expression
+a--  // post-decrement expression
+a in b  // membership check expression
+a.b  // property read expression
+a?.b  // conditional (non-emptiness) property read expression
+a[b]  // read property via key expression
+a[b]  // indexed element read expression
+a?.[b]  // conditional (non-emptiness) indexed element read expression
+a()  // function call expression
+a?.()  // conditional (non-emptiness) function call expression
+a instanceof b  // inheritance check expression
+a.b()  // method call expression
+a[b]()  // method call via key expression
+a.b = c  // property assignment expression
+a[b] = c  // property assignment via key (index) expression
+delete a  // global variable deletion expression
+delete a.b  // property deletion expression
+delete a[b]  // property deletion via key (index) expression
+a(b); a(b, c)  // function call expression with arguments
+a.b(c); a.b(c, d)  // method call expression with arguments
+new A; new A()  // object instantiation expression
+super()  // extended class instantiation expression
+import(a)  // module import expression
+a << b  // bit shift left expression
+a >> b  // bit shift right expression
+a >>> b  // bit shift right expression with sign drop
+a & b  // bitwise AND expression
+a | b  // bitwise OR expression
+a ^ b  // bitwise XOR expression
+~a  // bitwise NOT expression
+a <<= b  // bit shift left reassignment expression 
+a >>= b  // bit shift right reassignment expression
+a &= b  // bitwise AND reassignment expression
+a |= b  // bitwise OR reassignment expression
+a ^= b;  // bitwise XOR reassignment expression
+[...a]  // array creation expression with values from iterable
+(
+{...a}  // object creation expression with properties from object
+)  
+a(...b)  // function call expression with iterable arguments spread
+a.b(...c);  // method call expression with iterable arguments spread
+[a] = b; [a, b] = c;  // iterable destructuring expression
+[,, a] = b;  // iterable destructuring expression, skipping elements in the beginning
+[a,...b] = c  // iterable destructuring expression with rest of values collected in array
+(
+{a} = b, {a, b} = c  // object destructuring expression
+)(
+{a, ...b} = c  // object destructuring expression with rest of properties collected in object
+)(
+{a: b} = c  // object destructuring expression with differently named property/variable
+)
+[[a]] = b; [{a}] = b, {a: [b]} = c, {a: {b}} = c  // deep iterable/object destructuring expression
